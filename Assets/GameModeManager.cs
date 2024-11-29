068ac14c1de1ffc473731f525fdba312a0c4fda2
lanatodorovski <lana.todorovski@gmail.com>
2024-11-29 11:18:27 +0100

    Material and SO fix

---
+++ Assets/GameModeManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeManager : MonoBehaviour
+{
+    [SerializeField] private bool isScouting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
