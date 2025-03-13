febe474a1e68874a099493168b4c7f5450af6683
lanatodorovski <lana.todorovski@gmail.com>
2025-03-13 18:27:05 +0100

    full timer, gather completion ui not finished

--- Assets/Scripts/ExpeditionTimer.cs
+++ Assets/Scripts/ExpeditionTimer.cs
@@ -8,12 +8,12 @@ public class ExpeditionTimer : MonoBehaviour
 {
     [SerializeField] private float minuteTimeCounter = 3.0f;
     private float currentTime;
+
     [SerializeField] private TextMeshProUGUI timerText;
     // Start is called before the first frame update
     private void Awake()
     {
-        currentTime = Mathf.Floor(minuteTimeCounter) * 60 + 100 * (minuteTimeCounter % 1);
-        
+        currentTime = Mathf.Floor(minuteTimeCounter) * 60 + 100 * (minuteTimeCounter % 1);        
     }
     void Start()
     {
@@ -23,22 +23,32 @@ public class ExpeditionTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(currentTime > 0) UpdateCounter();
+        if (currentTime > 0)
+        {
+            UpdateCounter();
+            UpdateUI();
+        }
     }
 
     private void UpdateCounter()
     {
-        currentTime -= Time.deltaTime;
-        if (timerText != null) timerText.text = currentTime.ToString();
+        currentTime -= Time.deltaTime;        
         if (currentTime <= 0)
         {
             EndExpedition();
         }
     }
+    private void UpdateUI()
+    {
+        int outputMinutes = (int) currentTime / 60;
+        int outputSeconds = (int) currentTime % 60;
+        if (timerText != null) timerText.text = $"{outputMinutes:D2}:{outputSeconds:D2}"; // Formating in 00:00 form
+    }
 
     private void EndExpedition()
     {
         if (timerText != null) timerText.text = "Time has ended";
+        FindAnyObjectByType<MenuManager>().ToggleLevelCompletionUI();
     }
 
     private void OnValidate()
